aa02309df44e4a73a49dd7ec3a0fa919bd6681d3
Anka <runner@Mac-1726962539612.local>
2024-09-22 00:14:05 +0000

    Updated Package.swift and sources for latest Realm sdks

--- Package.swift
+++ Package.swift
@@ -17,13 +17,13 @@ let package = Package(
     targets: [
             .binaryTarget(
       name: "Realm",
-      url: "https://github.com/timstudt/RealmBinary/releases/download/v10.40.0/Realm.xcframework.zip",
-      checksum: "00c1feb0b1851c82753db208cabec04a49f54117f061557a1891fbe191b67d8f"
+      url: "https://github.com/timstudt/RealmBinary/releases/download/v10.54.0/Realm.xcframework.zip",
+      checksum: "1f1116d2e05643b569609082d142d33b7da9807b07f16702a3f15e9fb627e866"
     ),
     .binaryTarget(
       name: "RealmSwift",
-      url: "https://github.com/timstudt/RealmBinary/releases/download/v10.40.0/RealmSwift.xcframework.zip",
-      checksum: "d4b560f892afe7b172af1d9c667d29a08607f4cb62043be1ad04dac125f5fc3f"
+      url: "https://github.com/timstudt/RealmBinary/releases/download/v10.54.0/RealmSwift.xcframework.zip",
+      checksum: "d9a402f9bb75f5f82b5099d47321ae36fda202a13d5a781d33f63bd67cd1a747"
     )
     ]
 )
